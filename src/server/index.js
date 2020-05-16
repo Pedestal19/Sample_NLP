3100f79afefa787d6572c70263004893a9ff7806
Pedestal19 <gabeoji01@gmail.com>
2020-05-17 00:07:59 +0100

    coded logic for index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,51 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Aylien = require("aylien_textapi");
 
-app.use(express.static('dist'))
+const PORT = 8000;
 
-console.log(__dirname)
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("dist"));
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+// console.log("app id", process.env.AYLIEN_APP_ID);
+// console.log("app key", process.env.AYLIEN_APP_KEY);
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// const textApi = new Aylien({
+//   application_id: "68837b99",
+//   application_key: "0c18469543844e03b477be1a884e87bc"
+// });
+
+const textApi = new Aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
+
+app.get("/", (req, res) => res.sendFile("index.html"));
+
+app.post("/api", (req, res) => {
+  const { text } = req.body;
+  console.log("Request to '/api' endpoint", text);
+  textApi.sentiment({ text }, (error, result, remaining) => {
+    console.log("Aylien Callback", result, remaining);
+    res.send(result);
+  });
+});
+
+app.post("/article", (req, res) => {
+  const { text } = req.body;
+  console.log("Request to '/article' endpoint", text);
+  textApi.sentiment({ url: text }, (error, result, remaining) => {
+    console.log("Aylien Callback", result, remaining);
+    res.send(result);
+  });
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
