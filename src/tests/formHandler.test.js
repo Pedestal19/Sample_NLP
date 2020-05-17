e9afd824e6e5ce446fbba59dc7ce4a861dff3469
Pedestal19 <gabeoji01@gmail.com>
2020-05-17 04:48:17 +0100

    added tests

---
+++ src/tests/formHandler.test.js
@@ -0,0 +1,12 @@
+import {handleSubmit} from '../client/js/formHandler';
+
+describe('Form Handler Test', () => {
+    it('should be true if handleSubmit is defined', () => {
+        expect(handleSubmit).toBeDefined();
+    });
+
+    it('It should be true if handleSubmit is a function', () => {
+        expect(typeof handleSubmit).toBe('function');
+    });
+})
+
---
+++ src/tests/nameChecker.test.js
@@ -0,0 +1,12 @@
+import {checkForName} from '../client/js/nameChecker';
+
+
+describe('Check Name Test', () => {
+    it('should be true if checkForName is defined', () => {
+        expect(checkForName).toBeDefined();
+    });
+
+    it('It should be true if checkForName is a function', () => {
+        expect(typeof checkForName).toBe('function');
+    });
+})
