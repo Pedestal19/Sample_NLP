f1a015528f9a9a04e29fee8dbab803ca2e1fdc24
Pedestal19 <gabeoji01@gmail.com>
2020-05-17 03:30:47 +0100

    worked on ui and api calls

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,8 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+import "./styles/base.scss";
+import "./styles/main.scss";
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export * from "./js/runNLP";
+export * from "./js/nameChecker";
+export * from "./js/formHandler";
+
--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const Aylien = require("aylien_textapi");
 
-const PORT = 7777;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
@@ -15,15 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("dist"));
 
 
-// console.log("app id", process.env.AYLIEN_APP_ID);
-// console.log("app key", process.env.AYLIEN_APP_KEY);
-
-// const textApi = new Aylien({
-//   application_id: "68837b99",
-//   application_key: "0c18469543844e03b477be1a884e87bc"
-// });
-
-const textApi = new Aylien({
+const nlpApi = new Aylien({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY
 });
@@ -32,20 +24,20 @@ app.get("/", (req, res) => res.sendFile("index.html"));
 
 app.post("/api", (req, res) => {
   const { text } = req.body;
-  console.log("Request to '/api' endpoint", text);
-  textApi.sentiment({ text }, (error, result, remaining) => {
-    console.log("Aylien Callback", result, remaining);
+  console.log("Request to Aylien::", text);
+  nlpApi.sentiment({ text }, (error, result, remaining) => {
+    console.log("got this from API::", result, remaining);
     res.send(result);
   });
 });
 
 app.post("/article", (req, res) => {
   const { text } = req.body;
-  console.log("Request to '/article' endpoint", text);
-  textApi.sentiment({ url: text }, (error, result, remaining) => {
-    console.log("Aylien Callback", result, remaining);
+  console.log("Request to Aylien::", text);
+  nlpApi.sentiment({ url: text }, (error, result, remaining) => {
+    console.log("got this from API::", result, remaining);
     res.send(result);
   });
 });
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`App Running on Port: ${PORT}!`));
